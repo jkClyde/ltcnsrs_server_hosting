5fd40f1ffcb17d08cd5500c0f72f5f33258302c7
Clyde <jcayap18@gmail.com>
2024-01-28 19:40:19 +0800

    Updated CORS to allow all hosts 2

--- LTCNSRS/settings.py
+++ LTCNSRS/settings.py
@@ -167,13 +167,13 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    # "http://localhost:5173",  # Add your frontend URL here
-    # "https://ltcnsrs-server.onrender.com",
-    # "https://ltcnsrs.onrender.com",
-     "*",
+# CORS_ALLOWED_ORIGINS = [
+#     # "http://localhost:5173",  # Add your frontend URL here
+#     # "https://ltcnsrs-server.onrender.com",
+#     # "https://ltcnsrs.onrender.com",
+#      "*",
 
-]
+# ]
 
 
 
